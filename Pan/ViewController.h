4c41a2c085bc89a871f9cddd1f158005f7985d07
SWUCOMPUTER <swucomputer@SWUCOMPUTERui-MacBook-Air.local>
2017-04-04 13:51:16 +0900

    Initial Commit

---
+++ Pan/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Pan
+//
+//  Created by SWUCOMPUTER on 2017. 4. 4..
+//  Copyright © 2017년 Pan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Pan/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  Pan
+//
+//  Created by SWUCOMPUTER on 2017. 4. 4..
+//  Copyright © 2017년 Pan. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
